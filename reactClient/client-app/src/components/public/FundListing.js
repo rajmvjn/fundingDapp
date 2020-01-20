cb08271f34e94d19f3080c0ddf01ef62535bb56f
rajmvjn <rajmvjn@gmail.com>
2020-01-20 20:13:14 +0530

    fund listing added

--- reactClient/client-app/src/components/public/FundListing.js
+++ reactClient/client-app/src/components/public/FundListing.js
@@ -1,33 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 
 export const FundListing = () => {
 
     const [fundingRequests, setFundingRequests] = useState([{}]);
+    let res = [];
     
-    useEffect( () => {
-
-        let res = [];
+    useEffect( () => {        
 
         async function fetchData() {
 
             try {
-                res = await axios.get('http://localhost:4000/api/admin');
-                console.log(res.data);
+                res = await axios.get('http://localhost:4000/api/admin');                
+                setFundingRequests(res.data);
                 
             } catch (error) {
                 
             }
        
         }
-        fetchData();
+        fetchData();       
         
-        
-    });
-    
+    },[res.data]);
+
     return (
-        <div>
-            Fund listing..
-        </div>
+        <Fragment>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Project</th>
+                        <th scope="col">Fund Required</th>
+                        <th scope="col">Admin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                {
+                    fundingRequests.map( (fRequest, index) => {
+                        return(
+                            <tr key={index}>
+                                <th scope="row">{index+1}</th>
+                                <td>{ fRequest.fRName }</td>
+                                <td>{ fRequest.fund }</td>
+                                <td>{ fRequest.admin }</td>
+                            </tr> 
+                        )
+                    })
+                }                                       
+                </tbody>
+            </table>
+        </Fragment>
     )
 }
